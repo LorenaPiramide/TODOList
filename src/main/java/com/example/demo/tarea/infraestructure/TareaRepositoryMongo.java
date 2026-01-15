5ad0fb9303f1aef232cb21e82ea31541f48ac1bd
Lorena <230255@fppiramide.com>
2026-01-15 12:38:22 +0100

    Continuación

--- src/main/java/com/example/demo/tarea/infraestructure/TareaRepositoryMongo.java
+++ src/main/java/com/example/demo/tarea/infraestructure/TareaRepositoryMongo.java
@@ -1,4 +1,53 @@
 package com.example.demo.tarea.infraestructure;
 
-public class TareaRepositoryMongo {
+import com.example.demo.context.db.MongoDBConnector;
+import com.example.demo.tarea.domain.Tarea;
+import com.example.demo.tarea.domain.TareaRepository;
+import com.example.demo.usuario.domain.Usuario;
+import com.mongodb.client.result.InsertManyResult;
+import org.bson.Document;
+
+import java.util.List;
+
+public class TareaRepositoryMongo implements TareaRepository {
+
+    private final String collectionName = "tareas";
+
+    @Override
+    public void reset() {
+        MongoDBConnector.getDataBase().getCollection(collectionName).drop();
+    }
+
+    @Override
+    public void crearTarea(Tarea tarea, Usuario usuario) {
+        Document document = new Document();
+        document.append("texto", tarea.getTexto());
+        document.append("prioridad", tarea.getPrioridad());
+        document.append("fechaCreacion", tarea.getFechaCreacion());
+        document.append("fechaFinalizacion", tarea.getFechaFinalizacion());
+        document.append("estaCompletada", tarea.isEstaCompletada());
+        document.append("propietario", tarea.getPropietario());
+        document.append("usuariosAsignados", tarea.getUsuariosAsignados());
+        InsertManyResult result = MongoDBConnector.getDataBase().getCollection(collectionName).insertMany(document);
+    }
+
+    @Override
+    public List<Tarea> listarTareasUsuario(Usuario usuario) {
+        return List.of();
+    }
+
+    @Override
+    public void asignarTarea(Tarea tarea, Usuario propietario, Usuario asignado) {
+
+    }
+
+    @Override
+    public boolean cambiarEstado(Tarea tarea) {
+        return false;
+    }
+
+    @Override
+    public boolean editarDatos(Tarea tarea) {
+        return false;
+    }
 }
--- src/main/java/com/example/demo/tarea/infraestructure/TareaRestController.java
+++ src/main/java/com/example/demo/tarea/infraestructure/TareaRestController.java
@@ -1,4 +1,15 @@
 package com.example.demo.tarea.infraestructure;
 
+import com.example.demo.tarea.application.TareaUseCases;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tareas")
 public class TareaRestController {
+    private TareaUseCases tareaUseCases;
+
+    public TareaRestController(TareaUseCases tareaUseCases) {
+        this.tareaUseCases = new TareaUseCases(new TareaRepositoryMongo());
+    }
 }
